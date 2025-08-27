e9522a70cd7668f0bf1ec118a5741d2d94f0e445
NitikornNoraphaiphipaksa <noraphaiphipaksa@gmail.com>
2025-08-27 16:41:17 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,16 @@
-const CACHE = 'caltrack-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
+// sw.js (อัปเดตชื่อแคช)
+const CACHE = 'caltrack-v2';
+const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
-
 self.addEventListener('activate', e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
 });
-
 self.addEventListener('fetch', e=>{
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request).then(resp=>{
-      // cache new GET responses
       if(e.request.method === 'GET'){
         const clone = resp.clone();
         caches.open(CACHE).then(c=>c.put(e.request, clone)).catch(()=>{});
