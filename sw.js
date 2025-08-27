d0a07ce4c2664e4470449926b1420777f7a2114a
NitikornNoraphaiphipaksa <noraphaiphipaksa@gmail.com>
2025-08-27 16:21:03 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'caltrack-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).then(resp=>{
+      // cache new GET responses
+      if(e.request.method === 'GET'){
+        const clone = resp.clone();
+        caches.open(CACHE).then(c=>c.put(e.request, clone)).catch(()=>{});
+      }
+      return resp;
+    }).catch(()=>caches.match('./')))
+  );
+});
